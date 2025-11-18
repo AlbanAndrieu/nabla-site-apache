4122fd397a7bf92109c3f95e7cfc566e5eb9cc39
AlbanAndrieu <alban.andrieu@free.fr>
2025-11-18 12:23:31 +0100

    chore: Add vercel analytics

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-mixpanel.init('e28ee9625ec33982c6f8666ed974382c', { debug: true, ignore_dnt: true, track_pageview: true, persistence: 'localStorage' });
+mixpanel.init("e28ee9625ec33982c6f8666ed974382c", {
+	debug: true,
+	ignore_dnt: true,
+	track_pageview: true,
+	persistence: "localStorage",
+});
 
 // Set this to a unique identifier for the user performing the event.
 //mixpanel.identify(/* \"<USER_ID\"> */)
@@ -15,14 +20,16 @@ body.appendChild(button);
 //button.click();
 
 // 3. Add event handler
-button.addEventListener ("click", function() {
-  //alert("did something");
-  console.log("did something");
-  // Track a pageview.
-  mixpanel.track('testing that button was clidked');
+button.addEventListener("click", () => {
+	//alert("did something");
+	console.log("did something");
+	// Track a pageview.
+	mixpanel.track("testing that button was clidked");
 
-  // Track an event. It can be anything, but in this example, we're tracking a Sign Up event.
-  // mixpanel.track('Sign Up', {
-  //   'Signup Type': 'Referral'
-  // })
+	// Track an event. It can be anything, but in this example, we're tracking a Sign Up event.
+	// mixpanel.track('Sign Up', {
+	//   'Signup Type': 'Referral'
+	// })
 });
+
+import { inject } from "@vercel/analytics";
