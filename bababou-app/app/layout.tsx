58cfaff552769e02c35f67023b83fe2c507c4437
AlbanAndrieu <alban.andrieu@free.fr>
2025-11-19 22:02:37 +0100

    feat: Add Next Vercel application

--- bababou-app/app/layout.tsx
+++ bababou-app/app/layout.tsx
@@ -1,3 +1,6 @@
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
